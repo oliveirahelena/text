190e2f1a67a59072337a8e9f1812b9b34086b7f2
Helena Oliveira <helena@informach.com.br>
2026-01-30 18:13:23 -0300

    add text

--- src/script.js
+++ src/script.js
@@ -1,30 +1,90 @@
 import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+//import typefaceFont from 'three/examples/fonts/helvetiker_regular.typeface.json'
+
+/**
+ * Base
+ */
+
 // Canvas
-const canvas = document.querySelector("canvas.webgl")
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('textures/matcaps/8.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
 
-// Sizes
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+  '/fonts/helvetiker_regular.typeface.json',
+  (font) => {
+    // Material
+    const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+    // Text
+    const textGeometry = new TextGeometry(
+      'casaldevgames',
+      {
+        font: font,
+        size: 0.5,
+        depth: 0.2,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 5
+      }
+    )
+    textGeometry.center()
+
+    const text = new THREE.Mesh(textGeometry, material)
+    scene.add(text)
+
+    // Donuts
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+
+    for (let i = 0; i < 100; i++) {
+      const donut = new THREE.Mesh(donutGeometry, material)
+      donut.position.x = (Math.random() - 0.5) * 10
+      donut.position.y = (Math.random() - 0.5) * 10
+      donut.position.z = (Math.random() - 0.5) * 10
+      donut.rotation.x = Math.random() * Math.PI
+      donut.rotation.y = Math.random() * Math.PI
+      const scale = Math.random()
+      donut.scale.set(scale, scale, scale)
+
+      scene.add(donut)
+    }
+  }
+)
+
+/**
+ * Sizes
+ */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   // Update sizes
   sizes.width = window.innerWidth
   sizes.height = window.innerHeight
 
-  // Update Camera
+  // Update camera
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
 
@@ -33,56 +93,36 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-window.addEventListener('dblclick', () => {
-  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-  if (!fullscreenElement) {
-    if (canvas.requestFullscreen) {
-      canvas.requestFullscreen()
-    }
-    else if (canvas.webkitRequestFullscreen) {
-      canvas.webkitRequestFullscreen()
-    }
-  }
-  else {
-    if (document.exitFullscreen) {
-      document.exitFullscreen()
-    }
-    else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen()
-    }
-  }
-})
-
-// Camera
-// fov: campo de visualização: ângulo vertical em graus: ideal entre 45 e 75
-// aspect ratio: largura do renderer dividido pela altura do renderer
-// near: distância mínima que a câmera consegue enxergar o objeto
-// far: distância máxima que a câmera consegue enxergar o objeto
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-camera.lookAt(mesh.position)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
-
-  // Update objects
-  mesh.rotation.y = elapsedTime;
 
   // Update controls
   controls.update()
